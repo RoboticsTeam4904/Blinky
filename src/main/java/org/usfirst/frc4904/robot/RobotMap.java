1806e99cd1a541c92b2761d2b1bcd4ae8b94ac92
BenCantCode <benthegarvin@gmail.com>
2022-01-09 15:41:03 -0800

    Use correct ports

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -25,10 +25,16 @@ public class RobotMap {
         }
 
         public static class CANMotor {
+            /*
+            // FOUR MOTOR CONFIGURATION:
             public static int RIGHT_DRIVE_A = 2;
-            //public static int RIGHT_DRIVE_B = 3;
+            public static int RIGHT_DRIVE_B = 3;
             public static int LEFT_DRIVE_A = 4;
-            //public static int LEFT_DRIVE_B = 5;
+            public static int LEFT_DRIVE_B = 5;
+            */
+            // TWO MOTOR CONFIGURATION:
+            public static int RIGHT_DRIVE_A = 3;
+            public static int LEFT_DRIVE_A = 4;
         }
 
         public static class PWM {
