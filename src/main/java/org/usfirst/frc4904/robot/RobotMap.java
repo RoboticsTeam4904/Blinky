b51df3ab8cdd52cc341a09a7470bb1dba13976c2
amrrao <amrrao@nuevaschool.org>
2022-01-17 15:23:50 -0800

    deleted two motors

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -28,10 +28,8 @@ public class RobotMap {
         }
 
         public static class CANMotor {
-            public static int RIGHT_DRIVE_A = 2;
-            public static int RIGHT_DRIVE_B = 3;
+            public static int RIGHT_DRIVE_A = 3;
             public static int LEFT_DRIVE_A = 4;
-            public static int LEFT_DRIVE_B = 5;
             public static int EXTRA_MOTOR = -1; // TODO: set port
         }
 
@@ -88,9 +86,7 @@ public class RobotMap {
         public static EncoderPair chassisTalonEncoders;
         public static EncoderPair chassisCANCoders;
         public static Motor rightWheelA;
-        public static Motor rightWheelB;
         public static Motor leftWheelA;
-        public static Motor leftWheelB;
         public static SolenoidSubsystem solenoid;
         public static TankDrive chassis;
         public static CustomPIDController drivePID;
@@ -122,12 +118,10 @@ public class RobotMap {
 
         // Wheels
         CANTalonFX leftWheelATalon = new CANTalonFX(Port.CANMotor.LEFT_DRIVE_A);
-        CANTalonFX rightWheelATalon = new CANTalonFX(Port.CANMotor.RIGHT_DRIVE_B);
+        CANTalonFX rightWheelATalon = new CANTalonFX(Port.CANMotor.RIGHT_DRIVE_A);
 
         Component.rightWheelA = new Motor("rightWheelA", false, rightWheelATalon);
-        Component.rightWheelB = new Motor("rightWheelB", false, new CANTalonFX(Port.CANMotor.RIGHT_DRIVE_B));
         Component.leftWheelA = new Motor("leftWheelA", true, leftWheelATalon);
-        Component.leftWheelB = new Motor("leftWheelB", true, new CANTalonFX(Port.CANMotor.LEFT_DRIVE_B));
         Component.extraMotor = new Motor("extraMotor", false, new CANTalonFX(Port.CANMotor.EXTRA_MOTOR));
 
         // Wheel Encoders
@@ -150,8 +144,8 @@ public class RobotMap {
         Component.solenoid = new SolenoidSubsystem(Port.Pneumatics.SOLENOID.buildDoubleSolenoid());
 
         // General Chassis
-        Component.chassis = new TankDrive("Blinky-Chassis", Component.leftWheelA, Component.leftWheelB,
-                Component.rightWheelA, Component.rightWheelB);
+        Component.chassis = new TankDrive("Blinky-Chassis", Component.leftWheelA,
+                Component.rightWheelA);
         Component.chassis.setDefaultCommand(new ChassisMove(Component.chassis, new NathanGain()));
     }
 }
