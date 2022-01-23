f4b7ed27378d14df048b34cea01aa4a0215f3a1a
bcho04 <brandon.cho@gmail.com>
2022-01-23 15:33:17 -0800

    Change auton routine

--- src/main/java/org/usfirst/frc4904/auton/TestRoutine.java
+++ src/main/java/org/usfirst/frc4904/auton/TestRoutine.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4904.auton;
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisTurn;
 
@@ -9,9 +10,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class TestRoutine extends SequentialCommandGroup {
     public TestRoutine() {
         this.addCommands(
-            new ChassisMoveDistance(RobotMap.Component.chassis, 10D, null),
-            new ChassisTurn(RobotMap.Component.chassis, 3.14D/2D, null, null),
-            new ChassisMoveDistance(RobotMap.Component.chassis, 10D, null)
+            // new ChassisMoveDistance(RobotMap.Component.chassis, 10D, null),
+            // new ChassisTurn(RobotMap.Component.chassis, 3.14D/2D, null, null),
+            // new ChassisMoveDistance(RobotMap.Component.chassis, 10D, null)
+            new ChassisConstant(RobotMap.Component.chassis, 0, 0.2, 0, 4),
+            new ChassisConstant(RobotMap.Component.chassis, 0, -0.2, 0, 5),
+            new ChassisConstant(RobotMap.Component.chassis, 0, 0.2, 0, 1)
         );
     }
 }
