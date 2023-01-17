c6a052c03cd12d8894ad28174883cf3ed6577cbb
roboticsteam4904-2 <102199775+roboticsteam4904-2@users.noreply.github.com>
2023-01-16 16:00:19 -0800

    Reset standard commit and got pose

--- src/main/java/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
@@ -40,7 +40,7 @@ public class NathanGain extends Driver {
 	@Override
 	public void bindCommands() {
 		RobotMap.HumanInput.Driver.xbox.a.whenPressed(new SimpleSplines(RobotMap.Component.SplinesDrive,RobotMap.Component.SplinesDrive.getPose(), List.of(new Translation2d(1, 1), new Translation2d(-1, 2)), new Pose2d(0, 3, new Rotation2d(0)),5)); //change max voltage
-		RobotMap.HumanInput.Driver.xbox.b.whenPressed(new InstantCommand (() -> LogKitten.wtf(RobotMap.Component.SplinesDrive.getHeading())));
+		RobotMap.HumanInput.Driver.xbox.b.whenPressed(new InstantCommand (() -> LogKitten.wtf(RobotMap.Component.SplinesDrive.getPose())));
 	}
 
 	@Override
